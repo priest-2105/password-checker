98efde5976d84322fb2f020e8aa7b57dd418652d
priest-2105 <fawzybailey782@gmail.com>
2024-08-15 16:42:04 +0100

    styling ✨✨

---
+++ super.js
@@ -0,0 +1,15 @@
+
+
+// const openPassWordIcon = document.getElementById('openPassword');
+// const closePassWordIcon =document.getElementById('closePassword');
+
+
+// openPassWordIcon.addEventListener(click => {
+
+// const passwordInput = document.getElementById('passwordInput');
+// passwordInput.type = 'text';
+
+// })
+
+
+
