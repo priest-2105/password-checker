9d83f96fa27d5aa65eda39e847b2a4fcf947fa64
priest-2105 <fawzybailey782@gmail.com>
2024-08-15 17:28:40 +0100

    commit ✨✨

--- super.js
+++ super.js
@@ -1,15 +1,58 @@
 
 
-// const openPassWordIcon = document.getElementById('openPassword');
-// const closePassWordIcon =document.getElementById('closePassword');
+const openPassWordIcon = document.getElementById('openPassword');
+const closePassWordIcon =document.getElementById('closePassword');
+const passwordInput = document.getElementById('passwordInput');
 
 
-// openPassWordIcon.addEventListener(click => {
 
-// const passwordInput = document.getElementById('passwordInput');
-// passwordInput.type = 'text';
+// Event listener for the reveal password icon 
 
-// })
+openPassWordIcon.addEventListener('click', () => {
+
+// changes input type to text
+passwordInput.type = 'text';
+
+// reveals the hidepassword icon 
+closePassWordIcon.style.display = 'block';
+
+// hides the revealpassword icon 
+openPassWordIcon.style.display = 'none';
+
+})
 
 
+
+// Event listener for the hidepassword icon 
+
+closePassWordIcon.addEventListener('click', () => {
+
+// changes input type to password 
+passwordInput.type = 'password';
+
+// hides the hidepassword icon 
+closePassWordIcon.style.display = 'none';
+
+// reveals the revealpassword icon 
+openPassWordIcon.style.display = 'block';
+})
+
+
+
+
+passwordInput.addEventListener('change', () => {
+
+    const hasUpperCase = /[A-Z]/.text(password);
+    const hasLowerCase = /[a-z]/.text(password);
+    const hasNumber = /\d/.text(password);
+    const hasSymbol = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.text(password);
+
+    console.log(hasLowerCase);
+    
+
+    if (passwordInput.value.length <= 5){
+        document.getElementById('strengthLevelWarning').innerHTML = 'Password Is very Weak';
+    }
+
+})
 
