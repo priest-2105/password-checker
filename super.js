f93c6bc7b93c395e0f05eed3d5a88a5047611581
priest-2105 <fawzybailey782@gmail.com>
2024-08-15 23:28:01 +0100

    commt ✔✔

--- super.js
+++ super.js
@@ -42,16 +42,40 @@ openPassWordIcon.style.display = 'block';
 
 passwordInput.addEventListener('change', () => {
 
-    const hasUpperCase = /[A-Z]/.test(passwordInput.password);
-    const hasLowerCase = /[a-z]/.test(passwordInput.password);
-    const hasNumber = /\d/.test(passwordInput.password);
-    const hasSymbol = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(passwordInput.password);
+    const hasUpperCase = /[A-Z]/.test(passwordInput.value);
+    const hasLowerCase = /[a-z]/.test(passwordInput.value);
+    const hasNumber = /\d/.test(passwordInput.value);
+    const hasSymbol = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(passwordInput.value);
+    const hasSpace = /\s/.test(passwordInput.value)
 
-    console.log(hasLowerCase);
+
+        console.log('it has lower case ', hasLowerCase);
+        console.log('it has upper case ', hasUpperCase);
+        console.log('it has Number ', hasNumber);
+        console.log('it has symbol ', hasSymbol);
+        console.log('it has space',hasSpace);
+
+        if (passwordInput.value.length >= 12 ){
+        document.getElementById('characterMark').innerHTML = '✔';   
+        }else if(hasUpperCase ){
+        document.getElementById('uppercaseMark').innerHTML = '✔';   
+        }
+
     
+        
+
+    if (hasSpace){
+        document.getElementById('strengthLevelDanger').innerHTML = 'Password Cannot include Space !!';
+    }
+    if (hasSymbol >= 2  && hasNumber >= 2 && !hasSpace && hasLowerCase >= 8 && hasUpperCase >= 2 && passwordInput.value.length >= 15){
+        document.getElementById('strengthLevelSuccess').innerHTML = 'password is strong !!';
+    }else{
+        document.getElementById('strengthLevelWarning').innerHTML = 'password is not strong enough !!';   
+    }
+
 
     if (passwordInput.value.length <= 5){
-        document.getElementById('strengthLevelWarning').innerHTML = 'Password Is very Weak';
+        document.getElementById('strengthLevelWarning').innerHTML = 'Password Is very Weak !!';
     }
 
 })
